c507a19b2eba035889d35d9797138ad97b1e4d39
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-08-24 19:26:04 -0400

    style guide implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import TextPresets from "./components/TextPresets";
+
 const App = () => {
-  return <>App</>;
+  return (
+    <>
+      <TextPresets />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/TextPresets.tsx
@@ -0,0 +1,13 @@
+const TextPresets = () => {
+  return (     
+    <>
+      <div className="text-preset-1">Lorem ipsum dolor sit amet consectetuer</div>
+      <div className="text-preset-2">Lorem ipsum dolor sit amet consectetuer</div>
+      <div className="text-preset-3">Lorem ipsum dolor sit amet consectetuer</div>
+      <div className="text-preset-4">Lorem ipsum dolor sit amet consectetuer</div>
+      <div className="text-preset-4-semibold">Lorem ipsum dolor sit amet consectetuer</div>
+    </> 
+  );
+}
+ 
+export default TextPresets;
