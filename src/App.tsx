342a2e0cb2fe84a10cc5537371d70314781dd5a8
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-19 08:35:19 -0500

    scss setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <p className="text-preset--1">Your prince will find only death in the cold north.</p>
+      <p className="text-preset--2">Your prince will find only death in the cold north.</p>
+      <p className="text-preset--3">Your prince will find only death in the cold north.</p>
+      <p className="text-preset--4">Your prince will find only death in the cold north.</p>
+      <p className="text-preset--4-semibold">Your prince will find only death in the cold north.</p>
+    </div>
+  );
 }
