d16777a2599dde13c4b5118de81d4dfd915f1081
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-11-19 10:21:37 -0500

    app grid added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import css from "./App.module.scss";
+
 export default function App() {
   return (
-    <div>
-      <p className="text-preset--1">Your prince will find only death in the cold north.</p>
-      <p className="text-preset--2">Your prince will find only death in the cold north.</p>
-      <p className="text-preset--3">Your prince will find only death in the cold north.</p>
-      <p className="text-preset--4">Your prince will find only death in the cold north.</p>
-      <p className="text-preset--4-semibold">Your prince will find only death in the cold north.</p>
+    <div className={`${css.app} text-preset--3`}>
+      <div className={`${css.grid}`}>
+        <header>
+          <h1 className="text-preset--1">Desserts</h1>
+        </header>
+
+        <main>Main</main>
+
+        <aside>Aside</aside>
+      </div>
     </div>
   );
 }
